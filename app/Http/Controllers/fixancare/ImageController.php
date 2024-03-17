fde4ad8c93d8f161e0fd3f5f16b19fc09bc8066f
hisham.hsbr <hisham.hsbr@gmail.com>
2024-03-17 15:54:54 -0400

    some update v1

--- app/Http/Controllers/fixancare/ImageController.php
+++ app/Http/Controllers/fixancare/ImageController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\fixancare;
 
-use App\Models\fixancare\Image;
+use App\Models\Fixancare\Image;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Yajra\Datatables\Datatables;
